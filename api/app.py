18af62d15f4390094a1f3d38af7973c54d6e1515
wezlleyv <weslleyonline72@outlook.com>
2023-05-11 14:01:42 -0300

    initial api

---
+++ api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+import mysql.connector
+
+
+app = Flask(__name__)
+db = mysql.connector.connect(
+    host="sql10.freemysqlhosting.net",
+    user="sql10617616",
+    password="yBdi7EmpE4",
+    port="3306",
+)
+
+@app.route("/api/databases", methods=['GET'])
+def show_databases():
+    cursor = db.cursor()
+    cursor.execute("SHOW DATABASES")
+
+    return jsonify(cursor.fetchall())
+
+@app.route("/api/tables", methods=['GET'])
+def show_tables():
+    cursor = db.cursor()
+    cursor.execute(f"USE {request.args.get('database')}")
+    cursor.execute("SHOW TABLES")
+
+    return jsonify(cursor.fetchall())
+
+if __name__ == '__main__':
+    app.run(debug=True)
